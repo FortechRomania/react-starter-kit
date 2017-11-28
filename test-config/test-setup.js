5c5fc73cac638f1081292b2a9fca6d42c60c3df1
Alexandra Maria Pop <alexandrapop@Alexandras-Mac-mini.local>
2017-11-28 14:55:24 +0200

    Changed an import, deleted a file.

--- test-config/test-setup.js
+++ test-config/test-setup.js
@@ -1,5 +1,4 @@
-/* eslint import/first: off, no-unused-vars: off */
-import raf from "./tempPolyfills"; // not used, but needed in order to avoid console error
+import "raf/polyfill";
 import { configure } from "enzyme";
 import ReactSixteenAdapter from "enzyme-adapter-react-16";
 
