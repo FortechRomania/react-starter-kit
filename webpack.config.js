bf160f2c30d7985603642ae062c9b41f8721d2b0
Sergiu Ciulean <sergiuciulean@gmail.com>
2018-03-24 16:55:25 +0200

    reverted wrong commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,97 +1,62 @@
-const debug = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV !== "production";
 const webpack = require( "webpack" );
 const path = require( "path" );
-const ExtractTextPlugin = require( "extract-text-webpack-plugin" );
+const { BundleAnalyzerPlugin } = require( "webpack-bundle-analyzer" );
+const FriendlyErrorsWebpackPlugin = require( "friendly-errors-webpack-plugin" );
 
 const plugins = [
-    new webpack.optimize.CommonsChunkPlugin( {
-        name: "lib",
-        minChunks: Infinity,
-        filename: "[name].bundle.js",
-    } ),
-    new ExtractTextPlugin( {
-        filename: "[name].bundle.css",
-    } ),
+    new FriendlyErrorsWebpackPlugin(),
 ];
 
-if ( !debug ) {
+if ( !dev ) {
     plugins.push(
-        new webpack.optimize.DedupePlugin(),
         new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin( { mangle: false, sourcemap: false } ),
+        new BundleAnalyzerPlugin( {
+            analyzerMode: "static",
+            reportFilename: "webpack-report.html",
+            openAnalyzer: false,
+        } ),
     );
 }
 
-const scssRules = {
-    test: /\.scss$/,
-    use: ExtractTextPlugin.extract( {
-        use: [ {
-            loader: "css-loader",
-            options: {
-                url: false,
-                autoprefixer: false,
-                minimize: true,
-                // sourceMap: true
-            },
-        }, {
-            loader: "sass-loader",
-            options: {
-                // sourceMap: true,
-                includePaths: [ "styles" ],
-            },
-        } ],
-        fallback: "style-loader",
-    } ),
-};
-
 module.exports = {
+    mode: dev ? "development" : "production",
     context: path.join( __dirname, "src" ),
-    devtool: debug ? "inline-sourcemap" : false,
+    devtool: dev ? "none" : "source-map",
     entry: {
         app: "./js/App.js",
         lib: [ "react", "react-dom" ],
     },
-    devServer: {
-        historyApiFallback: true,
+    resolve: {
+        modules: [
+            path.resolve( "./src" ),
+            "node_modules",
+        ],
     },
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract( {
-                    fallback: "style-loader",
-                    use: "css-loader",
-                } ),
-            },
-            // {
-            //     test: /\.jsx?$/,
-            //     enforce: "pre",
-            //     exclude: /(node_modules|bower_components)/,
-            //     use: [
-            //         {
-            //             loader: "eslint-loader",
-            //             options: {
-            //                 failOnWarning: false,
-            //                 failOnError: true,
-            //             },
-            //         },
-            //     ],
-            // },
-            {
                 test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: "babel-loader",
-                /* query: {
-                    presets: [ "react", "es2015", "stage-0" ],
-                }, */
             },
-            scssRules,
         ],
     },
     output: {
         path: path.resolve( __dirname, "dist" ),
-        publicPath: "/",
-        filename: "[name].bundle.js",
+        filename: "js/[name].bundle.js",
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    chunks: "initial",
+                    minChunks: Infinity,
+                    name: "lib",
+                    enforce: true,
+                },
+            },
+        },
     },
     plugins,
 };
